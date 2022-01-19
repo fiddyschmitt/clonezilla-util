78e516ae133cc2007831d39ffba79df82c109fea
fiddyschmitt <fiddyschmitt@gmail.com>
2022-01-20 00:43:35 +1000

    Updated command line arg description

--- clonezilla-util/CL/Verbs/BaseVerb.cs
+++ clonezilla-util/CL/Verbs/BaseVerb.cs
@@ -7,7 +7,7 @@ namespace clonezilla_util.CL.Verbs
 {
     public class BaseVerb
     {
-        [Option('i', "input", HelpText = "The folder containing the Clonezilla archive. Or, a partclone filename.", Required = true, Min = 1)]
+        [Option('i', "input", HelpText = "The folder containing the Clonezilla archive. Or, a partclone filename, or image filename.", Required = true, Min = 1)]
         public IEnumerable<string>? InputPaths { get; set; }
 
         [Option("temp-folder", HelpText = "The folder to store temporary files (if required)", Required = false)]
