6716112dc82e0ca6224c94ef94daf180a12dcc11
fidel-planbuddies <100843168+fidel-planbuddies@users.noreply.github.com>
2023-11-18 16:05:47 +1000

    Update version number

--- clonezilla-util/Program.cs
+++ clonezilla-util/Program.cs
@@ -18,7 +18,7 @@ namespace clonezilla_util
     public class Program
     {
         const string PROGRAM_NAME = "clonezilla-util";
-        const string PROGRAM_VERSION = "2.0.4";
+        const string PROGRAM_VERSION = "2.1.0";
 
         private enum ReturnCode
         {
