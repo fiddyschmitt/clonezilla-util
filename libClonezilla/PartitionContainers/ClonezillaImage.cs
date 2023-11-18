ba745208307e94b9626b4d3b751517fbd6e43bc6
fidel-planbuddies <100843168+fidel-planbuddies@users.noreply.github.com>
2023-11-18 15:43:41 +1000

    Apply code suggestions

--- libClonezilla/PartitionContainers/ClonezillaImage.cs
+++ libClonezilla/PartitionContainers/ClonezillaImage.cs
@@ -142,10 +142,7 @@ namespace libClonezilla.PartitionContainers
         {
             get
             {
-                if (containerName == null)
-                {
-                    containerName = Path.GetFileName(ClonezillaArchiveFolder) ?? throw new Exception($"Could not get container name from path: {ClonezillaArchiveFolder}");
-                }
+                containerName ??= Path.GetFileName(ClonezillaArchiveFolder) ?? throw new Exception($"Could not get container name from path: {ClonezillaArchiveFolder}");
 
                 return containerName;
             }
